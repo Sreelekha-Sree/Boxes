089794a50942e4ca1d2cb74d0f778a0ee09f2f4e
Sreelekha <117349474+Sreelekha-Sree@users.noreply.github.com>
2023-01-06 23:25:44 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+import './index.css'
+const Boxes = () => 
+    (
+    <div className="bg-container">
+    <div className="main-container">
+        <h1 className="head">Boxes</h1>
+        <div className="box-container">
+          <div className="small-box">
+            <p className="name">Small</p>
+          </div>
+          <div className="medium-box">
+            <p className="name">Medium</p>
+          </div>
+          <div className="large-box">
+            <p className="name">Large</p>
+          </div>
+        </div>
+      </div>
+     </div>
+    )
+  export default  Boxes
+  
